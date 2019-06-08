4d2ade7b4427b05332c519dd3ad7d1280c159624
Chris Manson <chris@manson.ie>
2019-06-08 16:39:04 +0100

    updating to empress-blog

--- config/environment.js
+++ config/environment.js
@@ -33,6 +33,16 @@ module.exports = function(environment) {
         label: 'Home',
         route: 'index'
       }]
+    },
+
+    'responsive-image': {
+      sourceDir: 'images',
+      destinationDir: 'responsive-images',
+      quality: 80,
+      supportedWidths: [2000, 1000, 600, 300],
+      removeSourceDir: false,
+      justCopy: false,
+      extensions: ['jpg', 'jpeg', 'png', 'gif']
     }
   };
 
---
+++ tag/resolutions.md
@@ -0,0 +1,5 @@
+---
+name: Resolutions
+image:
+imageMeta:
+---
