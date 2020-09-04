89bb8d8e1ea8a196cbe4ca8630b1180101bf0a77
Chris Manson <chris@manson.ie>
2020-09-04 13:18:26 +0100

    v3.10.1...v3.21.0

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,5 +1,5 @@
-import Application from '../app';
-import config from '../config/environment';
+import Application from 'coders-log/app';
+import config from 'coders-log/config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
 
